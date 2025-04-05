999a65b014746a063b003cf1db5eba42eb2eaa67
Kascabrel <107616396+Kascabrel@users.noreply.github.com>
2025-04-05 19:14:06 +0200

    the change text in the welcome page

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello guy, This is an example of flask App that ar deployed using github actions and docker. '
+    return 'Hello guy, This is an example of flask App that ar deployed using github actions. '
 
 
-if __name__ == '__main__':
-    app.run(debug=True)

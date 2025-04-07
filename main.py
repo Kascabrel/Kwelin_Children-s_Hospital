008d2b6360ae674b75086eb0aba671494c589fa5
Kascabrel <107616396+Kascabrel@users.noreply.github.com>
2025-04-07 09:51:34 +0200

    fixed error of SSH key

--- main.py
+++ main.py
@@ -5,10 +5,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-<<<<<<< HEAD
-    return 'Hello guy. '
-=======
     return 'Hello guy, This is an example of flask App that.'
->>>>>>> 540bf31131665393ade7cd21ddbf91837d414b33
-
-

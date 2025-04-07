0ba8eb017be1e94381c0893823013e1bc3287e8c
Kascabrel <107616396+Kascabrel@users.noreply.github.com>
2025-04-07 12:38:37 +0200

    add 2 other endpoints

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello guy, This is an example of flask App.'
+
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+
+
+@app.route('/goodbye')
+def goodbye():
+    return 'Goodbye, World!'
